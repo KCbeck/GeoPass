4fe2cd4b2f4e16acf1a37950cdf5cb0805470e48
KCbeck <50602600+KCbeck@users.noreply.github.com>
2019-11-09 20:44:44 -0600

    removed Yarnlock

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { NavLink as RouterNavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import './NavBar.css';
 import {
   Collapse,
   Container,
--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,6 @@
 console.log ("keys loaded");
 
  export default {
-     apiweather: process.env.REACT_APP_APIkeyWeather,
+     apiweather: process.env.REACT_APP_APIkeyWheather,
      holiday: process.env.REACT_APP_apikey_holiday,
  }
